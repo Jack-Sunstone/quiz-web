91127832b4bf7365f9f0b00a8adc69434ea640cc
Jack Gander <jack@sunstone-systems.com>
2023-08-30 13:58:47 +0100

    Changed back-end URL

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,8 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://localhost:8001/';
+    service.apiBase = 'https://quiz-api-jgc.us-west-2.elasticbeanstalk.com/';
 
     return service;
 }]);
+
